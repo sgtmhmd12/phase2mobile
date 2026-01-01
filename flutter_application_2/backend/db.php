e0617c7dd77cad16a96de927184267f98cc7fad0
mohamed <32130493@students.liu.edu.lb>
2026-01-02 00:41:22 +0200

    fdfd

--- flutter_application_2/backend/db.php
+++ flutter_application_2/backend/db.php
@@ -1,9 +1,9 @@
 <?php
 $host = "mainline.proxy.rlwy.net";
-$user = "root";
-$pass = "HXIovQcKJlVKbryWaFqSeCyoGLOzSDdj";
-$db   = "railway";
-$port = 28507;
+$user = "root";              // <-- value of MYSQLUSER
+$pass = "HXIovQcKJlVKbryWaFqSeCyoGLOzSDdj";  // <-- value of MYSQLPASSWORD
+$db   = "railway";              // <-- value of MYSQLDATABASE
+$port = 3306;                  // <-- value of MYSQLPORT
 
 $conn = new mysqli($host, $user, $pass, $db, $port);
 
