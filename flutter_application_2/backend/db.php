8e095bbc4bdc9d3f1df22a7b219f028c0f2574a0
mohamed <32130493@students.liu.edu.lb>
2026-01-02 00:35:50 +0200

    fdfd

--- flutter_application_2/backend/db.php
+++ flutter_application_2/backend/db.php
@@ -1,12 +1,13 @@
 <?php
-$conn = new mysqli(
-  getenv("mysql.railway.internal"),
-  getenv("root"),
-  getenv("afZKYPhCCxDWFCcUURzZSAAvHgluURLL"),
-  getenv("railway"),
-  getenv("3306")
-);
+$host = "mainline.proxy.rlwy.net";
+$user = "root";
+$pass = "HXIovQcKJlVKbryWaFqSeCyoGLOzSDdj";
+$db   = "railway";
+$port = 28507;
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
-  die("DB connection failed");
+    die("DB connection failed: " . $conn->connect_error);
 }
+?>
