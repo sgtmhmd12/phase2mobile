5b1f87669a9254ae935ff1beedcee92af3b61449
mohamed <32130493@students.liu.edu.lb>
2026-01-02 02:20:03 +0200

    fdfd

--- flutter_application_2/backend/db.php
+++ flutter_application_2/backend/db.php
@@ -1,13 +1,21 @@
 <?php
-$host = "mysql-yvl7.railway.internal ";
-$user = "root";
-$pass = "LVnCNImdjThTsCUVeTamnaSdalgzohuP ";
-$db   = "railway";
-$port = 3306;
+$publicUrl = $_ENV["MYSQL_PUBLIC_URL"];
+
+/*
+ Example:
+ mysql://root:password@containers-us-west-123.railway.app:6543/railway
+*/
+
+$parts = parse_url($publicUrl);
+
+$host = $parts["host"];
+$user = $parts["user"];
+$pass = $parts["pass"];
+$db   = ltrim($parts["path"], "/");
+$port = $parts["port"];
 
 $conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
-    die("DB connection failed: " . $conn->connect_error);
+    die(json_encode(["error" => $conn->connect_error]));
 }
-?>
