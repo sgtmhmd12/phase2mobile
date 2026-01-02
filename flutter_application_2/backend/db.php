0d180729de763b949049e3b5fae5451ff743ad3e
mohamed <32130493@students.liu.edu.lb>
2026-01-02 02:14:51 +0200

    fdfd

--- flutter_application_2/backend/db.php
+++ flutter_application_2/backend/db.php
@@ -1,16 +1,13 @@
 <?php
-$host = $_ENV["MYSQLHOST"];
-$user = $_ENV["MYSQLUSER"];
-$pass = $_ENV["MYSQLPASSWORD"];
-$db   = $_ENV["MYSQLDATABASE"];
-$port = $_ENV["MYSQLPORT"];
+$host = "mysql-yvl7.railway.internal ";
+$user = "root";
+$pass = "LVnCNImdjThTsCUVeTamnaSdalgzohuP ";
+$db   = "railway";
+$port = 3306;
 
 $conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
-    die(json_encode([
-        "db_error" => $conn->connect_error,
-        "host" => $host,
-        "db" => $db
-    ]));
+    die("DB connection failed: " . $conn->connect_error);
 }
+?>
